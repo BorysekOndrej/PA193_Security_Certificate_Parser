b01877fe39970af17da81dece6c929b9459cdcac
Ondřej Borýsek <BorysekOndrej@users.noreply.github.com>
2021-04-06 18:05:54 +0200

    Modify BibliographyParser for PropertyParserInterface

--- BibliographyParser.py
+++ BibliographyParser.py
@@ -1,9 +1,11 @@
 import re
 from typing import List, Dict
+from PropertyParserInterface import PropertyParserInterface
 
-class BibliographyParser:
+
+class BibliographyParser(PropertyParserInterface):
     """ Pibliography parser. Works with list of lines from the text file. """
-    def __init__(self):
+    def __init__(self, lines: List[str]):
         """ Initialize the parser, set parsing settings """
         # This bibliography parsing regex works as follows:
         # (\[([^\s]*)\]) group tries to find a left square braket '[' with any character(s) inside the brakets 
@@ -13,6 +15,7 @@ class BibliographyParser:
         # The last regex group - ([\s\S]*?(?=\n{2,}|\n[^\S\n]*\n|\[))") tries to find any sequence of characters 
         # including newline and spaces. But it is limited to two the newline delimiters or empty line -\n[^\S\n]*\n,
         # or just left square braket '['.
+        super().__init__(lines)
         self.globalPattern = re.compile(r"(\[([^\s]*)\])[^\S\n]{2,}([\s\S]*?(?=\n{2,}|\n[^\S\n]*\n|\[))")
         # Removes redundant spaces, tabs and new lines
         self.postprocessPattern = re.compile(r"\s+")
@@ -32,13 +35,13 @@ class BibliographyParser:
         """ Remove all redundant whitespaces and newline characters. """
         return re.sub(self.postprocessPattern, ' ', string).rstrip()
 
-    def parse(self, input_lines: List[str]) -> Dict[str, str]:
+    def parse(self):
         """ Parse input text and find bibliography. """
         result: Dict[str, str] = {}
-        text = self.preprocess(input_lines)
+        text = self.preprocess(self.lines)
         bibliography = re.findall(self.globalPattern, text)
         for b in bibliography:
             # Here we take the third element of the tuple defined by the regex
             processed = self.postprocess(b[2])
             result[b[0]] = processed
-        return result
+        self.result = result
