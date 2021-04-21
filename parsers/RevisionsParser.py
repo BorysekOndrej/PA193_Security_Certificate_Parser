82008afcf8182bbd9750d7d755b24ab2303b6248
Michael Klunko <michael.klunko@gmail.com>
2021-04-21 22:58:53 +0200

    Add keywords index search

--- parsers/RevisionsParser.py
+++ parsers/RevisionsParser.py
@@ -8,5 +8,15 @@ class RevisionsParser(PropertyParserInterface):
     def __init__(self, lines: List[str]):
         super().__init__(lines)
 
+    def find_rev(self, lines: List[str]):
+        subs = ['Revision History', 'Version Control']
+        subs = [string.lower() for string in subs]
+        # Find line indexes where the substring is
+        start_idxs = [i for i, line in enumerate(lines) if any(map(line.lower().__contains__, subs))]
+        print(start_idxs)
+
+
+
     def parse(self) -> List[dict]:
+        self.find_rev(self.lines)
         return []
