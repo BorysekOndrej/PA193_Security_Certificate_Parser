a893f807e42d092b2029921f4e8b553006845d55
Michael Klunko <michael.klunko@gmail.com>
2021-04-23 23:20:18 +0200

    Add two-column table parsing

--- parsers/RevisionsParser.py
+++ parsers/RevisionsParser.py
@@ -9,6 +9,8 @@ class RevisionsParser(PropertyParserInterface):
         super().__init__(lines)
         # Pattern to detect date in any format
         self.datePattern = re.compile(r"[0-9]{2,4}[\/\-\.]([0-9]{2}|[A-Za-z]+)[\/\-\.][0-9]{2,4}")
+        # Pattern to detect possible two columns in one line
+        self.twoColumns = re.compile(r"^[ ]{0,5}(\S+(?:(?!\s{2}).)+)[\s]{3,}(\S+(?:(?!\s{2}).)+)$")
 
     def find_keywords(self, lines: List[str], keywords: List[str]) -> list:
         subs = [string.lower() for string in keywords]
@@ -16,19 +18,66 @@ class RevisionsParser(PropertyParserInterface):
         start_idxs = [i for i, line in enumerate(lines) if any(map(line.lower().__contains__, subs))]
         return start_idxs
 
-    def get_date(self, line: str) -> str, int:
-        res = [m for m in re.finditer(pattern, string)]
+#TODO: fix syntax, add return types
+    def get_date(self, line: str):
+        res = [m for m in re.finditer(pattern, line)]
         # Usually string should contain only one date
+        # TODO: add support for no date.
         index = res[0].start(0)
         date_str = res[0].group(0)
         return date_str, index
 
+    def is_table_header(self, string: str, keywords: List[str]):
+        """ Check line if it can be a table header """
+        score = 0
+        columns = re.split(r" {2,}", string)
+        if len(columns) > 4 or len(columns) < 2:
+            return False
+        # Check if does not contain numbers or symbols
+        is_alpha_or_space = all(c.isalpha() or c.isspace() for c in string)
+        if not is_alpha_or_space: return False 
+        # Keywords score TODO: add some logic for scoring
+        #[score := score + 1 for keyword in keywords if(keyword in string2)]
+        return True
+
+    def extract_formatting_params(self, line: str) -> List[int]:
+        """ Tries to extract start indexes for each column """
+        # Split line by two or more spaces
+        columns = re.split(r" {2,}", line)
+        # We need to find index of each column in string.
+        # As split does not give indexes, let's try to find by first occurence
+        indexes = [line.find(column) for column in columns]
+        return indexes
+
+    def extract_two_column_table(self, lines: List[str]):
+        """ Tries to extract two-column table """
+        table_header_keywords = ['Date', 'Description', 'Rev', 'Revision', 'Version']
+        table_header = ""
+        table = []
+        # TODO: work more with false lines
+        false_lines = 0
+        for line in lines:
+            result = re.findall(self.twoColumns, line)
+            if false_lines > 5: return []
+            if len(result) == 0:
+                false_lines += 1
+                continue
+            # TODO: work more with header. What if no header? 
+            if self.is_table_header(line, table_header_keywords):
+                table_header = line
+            # TODO: if header found no need to add
+            table.append(line)
+            
+
     def find_table(self, start: List[int], lines: List[str]):
         for id in start:
-            pass
+            # We take next line after found keyword
+            next_line = id + 1
+            self.extract_two_column_table(lines[next_line:])
 
 
     def parse(self) -> List[dict]:
         keywords = ['Revision History', 'Version Control']
         indexes = self.find_keywords(self.lines, keywords)
+        self.find_table(indexes, self.lines)
         return []
